4bfd978c0e566e27ffd17440abea99e537a88303
Liam Heynderickx <liamheynderickx21@gmail.com>
2024-11-20 10:40:33 +0100

    WebUpload

---
+++ frontend/app/page.tsx
@@ -0,0 +1,14 @@
+'use client'
+import React from 'react';
+import GameStart from './HomeComponents/GameStart';
+import Rules from './HomeComponents/rules';
+
+export default function Home() {
+  return (
+    <main>
+      <GameStart />
+      <Rules />
+  </main>
+  );
+};
+
